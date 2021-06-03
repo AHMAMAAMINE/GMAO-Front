91c49b285e3b6ae1a3f796f3034138814b7588e1
Mehdi <elwafi.courrier99@gmail.com>
2021-06-03 20:44:05 +0100

    Added intervention-calander logic

--- src/app/controller/model/intervention.model.ts
+++ src/app/controller/model/intervention.model.ts
@@ -1,18 +1,18 @@
-import { EtatIntervention } from './etat-intervention.model';
-import { MateraialIntervention } from './materaial-intervention.model';
-import {Conseils} from './conseils.model';
-import {InterventionMembreEquipe} from './intervention-membre-equipe.model';
+import { EtatIntervention } from "./etat-intervention.model";
+import { MateraialIntervention } from "./materaial-intervention.model";
+import { Conseils } from "./conseils.model";
+import { InterventionMembreEquipe } from "./intervention-membre-equipe.model";
 
 export class Intervention {
   public id: number;
   public dateDeProbleme: Date;
-  public dateDebut: Date;
-  public dateFin: Date;
+  public dateDebut: string;
+  public dateFin: string;
   public description: string;
   public libelle: string;
   public code: string;
   public etatIntervention = new EtatIntervention();
   public interventionMembreEquipe = new Array<InterventionMembreEquipe>();
   public materaialInterventions = new Array<MateraialIntervention>();
-  public  conseils = new Array<Conseils>();
+  public conseils = new Array<Conseils>();
 }
--- src/app/view/admin/stock/stock.component.ts
+++ src/app/view/admin/stock/stock.component.ts
@@ -2,6 +2,9 @@ import { EquipesService } from "./../../../controller/service/equipes.service";
 import { Intervention } from "./../../../controller/model/intervention.model";
 import { InterventionService } from "./../../../controller/service/intervention.service";
 import { Component, OnInit } from "@angular/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 @Component({
   selector: "app-stock",
@@ -13,22 +16,53 @@ export class StockComponent implements OnInit {
     private interventionService: InterventionService,
     private equipesService: EquipesService
   ) {}
-
+  public fullcalendarOptions: any;
+  public events: any;
   public intervetions: Array<Intervention>;
   public numInterventions: number;
   public numEquipes: number;
+  public interventionDto = new Array<Object>();
+
+  convertInterventions(Interventions: Array<Intervention>) {
+    let i = 0;
+    this.intervetions.forEach((elem) => {
+      ++i;
+      let obj = {
+        id: i,
+        title: elem.libelle,
+        start: elem.dateDebut.slice(0, 10),
+        end: elem.dateFin.slice(0, 10),
+      };
+      this.interventionDto.push(obj);
+    });
+  }
 
   ngOnInit(): void {
     this.interventionService.findAll().subscribe((data) => {
       if (data) {
         this.intervetions = data;
         this.numInterventions = data.length;
+        this.convertInterventions(data);
+
+        console.log(this.interventionDto);
+
+        this.fullcalendarOptions = {
+          plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+          defaultDate: "2021-05-30",
+          header: {
+            left: "prev,next",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          },
+          editable: true,
+        };
       }
     });
-    this.equipesService.findAll().subscribe((data) => {
-      if (data) {
-        this.numEquipes = data.length;
-      }
-    });
+
+    // this.equipesService.findAll().subscribe((data) => {
+    //   if (data) {
+    //     this.numEquipes = data.length;
+    //   }
+    // });
   }
 }
