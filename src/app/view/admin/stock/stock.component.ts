b4d5ffd0aab565f2dce046834099bfc29a81a164
Mehdi <elwafi.courrier99@gmail.com>
2021-06-07 19:38:39 +0100

    added to stock cmp

--- src/app/view/admin/stock/stock.component.ts
+++ src/app/view/admin/stock/stock.component.ts
@@ -19,7 +19,10 @@ export class StockComponent implements OnInit {
   public fullcalendarOptions: any;
   public events: any;
   public intervetions: Array<Intervention>;
-  public numInterventions: number;
+  public encoursInterventions: number;
+  public totalInterventions: number;
+  public data: any;
+
   public numEquipes: number;
   public interventionDto = {
     data: [],
@@ -29,9 +32,22 @@ export class StockComponent implements OnInit {
     let i = 0;
     this.intervetions.forEach((elem) => {
       ++i;
+      let color = "";
+      switch (elem.etatIntervention.couleur) {
+        case "rouge":
+          color = "red";
+          break;
+        case "vert":
+          color = "green";
+          break;
+
+        default:
+          color = "orange";
+          break;
+      }
       let obj = {
         id: i,
-        // color: "red",
+        color: color,
         title: elem.libelle,
         start: elem.dateDebut.slice(0, 10),
         end: elem.dateFin.slice(0, 10),
@@ -41,10 +57,25 @@ export class StockComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.data = {
+      labels: ["en cours", "complete", "hhehe"],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+          hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        },
+      ],
+    };
+
     this.interventionService.findAll().subscribe((data) => {
       if (data) {
         this.intervetions = data;
-        this.numInterventions = data.length;
+        this.totalInterventions = data.length;
+        this.encoursInterventions = data.filter((elem) => {
+          return elem.etatIntervention.couleur === "orange";
+        }).length;
+
         this.convertInterventions(data);
         this.events = this.interventionDto.data;
         console.log(this.interventionDto);
