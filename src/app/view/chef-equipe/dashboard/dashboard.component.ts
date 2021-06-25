0a42ddea165ee2053ec0379a291ac326a7c72249
Mehdi <elwafi.courrier99@gmail.com>
2021-06-25 16:06:02 +0100

    Updated CHef-Collab Dashboard

--- src/app/view/chef-equipe/dashboard/dashboard.component.ts
+++ src/app/view/chef-equipe/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {DemandeCongeService} from '../../../controller/service/demande-conge.service';
+import {TacheInterventionService} from '../../../controller/service/tache-intervention.service';
+import {CollaborateurService} from '../../../controller/service/collaborateur.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private demandeCongeService:DemandeCongeService,
+      private tacheService:TacheInterventionService,
+      private collabService:CollaborateurService
+  ) { }
+  private demandesNum:number;
+  private tacheNum:number;
+  private collabNum:number;
 
   ngOnInit(): void {
+    this.demandeCongeService.findAll().subscribe((data) => {
+      this.demandesNum = data.length;
+    });
+    this.tacheService.findAll().subscribe(data =>{
+      this.tacheNum = data.length;
+    });
+    this.collabService.findAll().subscribe(data =>{
+      this.collabNum = data.length;
+    });
   }
 
 }
